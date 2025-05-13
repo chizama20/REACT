6d7e3ba1032aa294da937dd89a1a566858d5dcc0
chizama20 <133240770+chizama20@users.noreply.github.com>
2025-05-13 10:44:58 -0400

    yes

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,17 +1,10 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import './compnents/links/Card.jsx'
-import Card from './compnents/links/Card.jsx'
-import Home from './compnents/Home.jsx'
+import './Card.jsx'
+import Card from './Card.jsx'
 
 function App() {
   return(
     <>
-      <Router>
-      <Routes>
-        <Route path="./compnents/Home" element={<Home />} />
-        <Route path="./compnents/links/Card" element={<Card />} />
-      </Routes>
-    </Router>
+      <Card></Card>
     </>
 
   );
