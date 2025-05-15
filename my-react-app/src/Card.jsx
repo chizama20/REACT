f1b970069ea794469bb6800bc9aba8e6b87dd75c
chizama20 <133240770+chizama20@users.noreply.github.com>
2025-05-15 09:44:26 -0400

    set color

--- my-react-app/src/Card.jsx
+++ my-react-app/src/Card.jsx
@@ -1,8 +1,26 @@
+import { useState } from "react";
 
 function Card(){
-    return(
-        <div className="card">
-            <a href="https://chizama.com">"Short of reasons why"</a>
+    const [color, setColor] = useState("#FFFFFF");
+
+    function changeColor(event){
+        setColor(event.target.value);
+    }
+
+    return(    
+        <div>
+            <h1>Color Picker</h1>
+            <div style={{backgroundColor: color}} className="set-color">
+                <p>
+                    Selected Color: {color}
+                </p>
+            </div>
+
+            <div className="set">
+                <p>Select a Color:</p>
+                <input type="color" onChange={changeColor}></input>
+            </div>
+
         </div>
     );
 }
